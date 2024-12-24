4d3cbf76d6223ac15f8033dbdc670ae7d64afbc6
boyqiu001 <boyqiu001@boyqiu001.cn>
2024-12-24 12:51:25 +0800

    更改日志为分片日志

--- logger.py
+++ logger.py
@@ -1,10 +1,17 @@
 import logging
-
+import logging.handlers
 def logger(name):
     logger = logging.getLogger(name)
 
     # 创建一个handler，用于写入日志文件
-    fh = logging.FileHandler('test.log')
+ # 日志文件路径
+    log_file = 'VRCTODGLAB.log'
+    # 单个日志文件最大大小
+    max_bytes = 1024 * 1024  # 1MB
+    # 最多保留的日志文件数
+    backup_count = 20
+    # 创建 file handler 并设置日志级别
+    fh = logging.handlers.RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
     fh.setLevel(logging.INFO)
     # 再创建一个handler，用于输出到控制台
     ch = logging.StreamHandler()
